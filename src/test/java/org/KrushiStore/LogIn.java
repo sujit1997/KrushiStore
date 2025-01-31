71e93e2d0a8dd19e2bffdc96d7cb91dac65f2bbc
sujit <sujitmanmode1997@gmail.com>
2025-01-31 15:18:14 +0530

    first commit

--- src/test/java/org/KrushiStore/LogIn.java
+++ src/test/java/org/KrushiStore/LogIn.java
@@ -1,8 +1,22 @@
 package org.KrushiStore;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class LogIn {
-
     public static void main(String[] args) {
-
+        WebDriver driver;
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.krushistore.com/");
+        driver.findElement(By.xpath("//*[contains(@class, 'w-[29px]')]")).click();
+        driver.findElement(By.xpath("/html/body/div[3]/div[3]/main/div/p[2]/span")).click();
+        driver.findElement(By.xpath("/html/body/div[3]/div[3]/main/div/form/div/div/input")).sendKeys("9325825024");
+        driver.findElement(By.xpath("/html/body/div[3]/div[3]/main/div/form/button")).click();
+        // driver.findElement(By.xpath("/html/body/header/div[2]/div/div[2]/div[2]/div/div[1]/svg")).click();
+        //driver.findElement(By.className("font-semibold cursor-pointer text-[#0066c0] no-underline hover:underline")).click();
     }
 }
--- src/test/java/org/KrushiStore/SignUp.java
+++ src/test/java/org/KrushiStore/SignUp.java
@@ -1,5 +1,6 @@
 package org.KrushiStore;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -13,10 +14,14 @@ import java.time.Duration;
     public class SignUp{
     public static void main(String[] args) {
         WebDriver driver;
+        WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.manage().window().maximize(); // Maximize the browser window
         driver.get("https://www.krushistore.com/"); // Navigate to the target URL
-        driver.findElement(By.xpath("//*[contain(@class, 'w-[29px]')]*")).click();
+        driver.findElement(By.xpath("//*[contains(@class, 'w-[29px]')]")).click();
+        driver.findElement(By.id("name")).sendKeys("sujit");
+        driver.findElement(By.id("mobile")).sendKeys("9325825024");
+        driver.findElement(By.xpath("/html/body/div[3]/div[3]/main/div/form/button")).click();
 
         // Use WebDriverWait with Duration for the timeout (new in Selenium 4.x)
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));// Set up the browser and navigate to the website
