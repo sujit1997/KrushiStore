62424345c7f26b557435faf18cebe8d2e257256f
sujit <sujitmanmode1997@gmail.com>
2025-01-31 21:01:12 +0530

    first commit

--- src/test/java/org/KrushiStore/LogIn.java
+++ src/test/java/org/KrushiStore/LogIn.java
@@ -4,12 +4,18 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class LogIn {
     public static void main(String[] args) {
         WebDriver driver;
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+        //WebDriverManager.chromedriver().setup(); //Its use for chrome driver
+        //driver = new ChromeDriver();
+       // WebDriverManager.firefoxdriver().setup(); //Its use for Firefox Driver
+        //driver = new FirefoxDriver();
+        WebDriverManager.edgedriver().setup(); // Its use for Microsoft Edge browser
+        driver = new EdgeDriver();
         driver.manage().window().maximize();
         driver.get("https://www.krushistore.com/");
         driver.findElement(By.xpath("//*[contains(@class, 'w-[29px]')]")).click();
