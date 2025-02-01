4909bae834de5bb365dde59c47e0d8f6732b7447
sujit <sujitmanmode1997@gmail.com>
2025-02-01 08:39:13 +0530

    first commit

--- src/test/java/org/KrushiStore/logInPageTest.java
+++ src/test/java/org/KrushiStore/logInPageTest.java
@@ -3,13 +3,18 @@ package org.KrushiStore;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import pages.logInPage;
+
+import java.time.Duration;
 
 public class logInPageTest {
     public static WebDriver driver = null;
+
     public static void main(String[] args) {
-        WebDriver driver;
+        //SETUP WEBDRIVER
         //WebDriverManager.chromedriver().setup(); //Its use for chrome driver
         //driver = new ChromeDriver();
         // WebDriverManager.firefoxdriver().setup(); //Its use for Firefox Driver
@@ -18,18 +23,28 @@ public class logInPageTest {
         driver = new EdgeDriver();
         driver.manage().window().maximize();
         driver.get("https://www.krushistore.com/");
-        driver.findElement(By.xpath("//*[contains(@class, 'w-[29px]')]")).click();
-        driver.findElement(By.xpath("/html/body/div[3]/div[3]/main/div/p[2]/span")).click();
-       // WebElement enternumber = driver.findElement(By.xpath("/html/body/div[3]/div[3]/main/div/form/div/div/input"));
-        //enternumber.sendKeys("9325825024");
-        logInPage.enterMobileNumber(driver).sendKeys("9325825024");
-        driver.findElement(By.xpath("/html/body/div[3]/div[3]/main/div/form/button")).click();
 
+        try {
 
+            //driver.findElement(By.xpath("//*[contains(@class, 'w-[29px]')]")).click();
+            logInPage.clickToSignUP(driver);
+
+            //driver.findElement(By.xpath("/html/body/div[3]/div[3]/main/div/p[2]/span")).click();
+            logInPage.clickToSignIn(driver);
+
+            // WebElement enternumber = driver.findElement(By.xpath("/html/body/div[3]/div[3]/main/div/form/div/div/input"));
+            //enternumber.sendKeys("9325825024");
+            logInPage.enterMobileNumber(driver).sendKeys("9325825024");
+
+            // driver.findElement(By.xpath("/html/body/div[3]/div[3]/main/div/form/button")).click();
+            logInPage.clickToRequestOTPButton(driver);
+            Thread.sleep(5000);
+        } catch (Exception e) {
+            System.out.println("Test failed :" + e.getMessage());
+        } finally {
+            driver.quit();
+        }
         // WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(1000));
-        // driver.close();
-
-
     }
-
 }
+
--- src/test/java/pages/logInPage.java
+++ src/test/java/pages/logInPage.java
@@ -5,10 +5,39 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 public class logInPage {
-     private static WebElement element = null;
+     //private static WebElement element = null;
 
-        public static WebElement enterMobileNumber(WebDriver driver){
-            element = driver.findElement(By.xpath("//input[@id = 'mobile']"));
-            return element;
+        private static By signUpLogoPath = By.xpath("//*[contains(@class, 'w-[29px]')]");
+        private static By signInPath = By.xpath("//span[@class='font-semibold cursor-pointer text-[#0066c0] no-underline hover:underline']");
+        private static By mobileNumber = By.xpath("//input[@id='mobile']");
+        private static By requestButton = By.xpath("//button[@type='submit']");
+
+        public static void clickToSignUP(WebDriver driver){
+            if (driver.findElements(signUpLogoPath).size() > 0) {
+                driver.findElement(signUpLogoPath).click();
+            } else {
+                System.out.println("Sign Up button not found!");
+            }
+         }
+
+         public static void clickToSignIn(WebDriver driver){
+             if (driver.findElements(signInPath).size() > 0) {
+                 driver.findElement(signInPath).click();
+             } else {
+                 System.out.println("Sign In button not found!");
+             }
+         }
+
+         public static WebElement enterMobileNumber(WebDriver driver){
+             return  driver.findElement(mobileNumber);
+
+        }
+
+        public static void clickToRequestOTPButton(WebDriver driver){
+            if (driver.findElements(requestButton).size() > 0) {
+                driver.findElement(requestButton).click();
+            } else {
+                System.out.println("Request OTP button not found!");
+            }
         }
 }
