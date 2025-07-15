0f328e9071693d90f20de2ab642b2db012f0c935
SUJIT <sujitmanmode1997@gmail.com>
2025-07-15 17:37:25 +0530

    first commit

--- src/main/java/UI_Use_Only_Selenium_TestNg/A_signIn.java
+++ src/main/java/UI_Use_Only_Selenium_TestNg/A_signIn.java
@@ -2,20 +2,28 @@ package UI_Use_Only_Selenium_TestNg;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class A_signIn {
+
+    public static String browser = "chrome";
+    public static WebDriver driver;
+
     public static void main(String[] args) {
-
-        WebDriverManager.chromedriver().setup();
-        // WebDriverManager.firefoxdriver().setup();
-        // WebDriverManager.edgedriver().setup();
-
-        ChromeDriver driver = new ChromeDriver();
-        // FirefoxDriver driver1 = new FirefoxDriver();
-        // EdgeDriver driver2 = new EdgeDriver();
+        if (browser.equals("firefox")){
+        WebDriverManager.firefoxdriver().setup();
+        driver = new FirefoxDriver();
+        } else if (browser.equals("chrome")) {
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+        } else if (browser.equals("edge")) {
+            WebDriverManager.edgedriver().setup();
+            driver = new EdgeDriver();
+        }
+        driver.manage().window().maximize();
 
         driver.get("https://www.krushistore.com/?srsltid=AfmBOoroHwdVOs36vsQ5VMVvTtfLCirAINjiIG35YFrM-1uGQEJ1D93F");
         driver.manage().window().maximize();
